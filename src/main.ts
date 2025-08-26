8fedb10dde823f13562a2808345b22c8bb6eceb5
yzy <yanziyi290@gmail.com>
2025-08-26 12:30:32 +0100

    test跨域

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@
  * @Author: yzy
  * @Date: 2025-08-19 21:45:37
  * @LastEditors: yzy
- * @LastEditTime: 2025-08-25 02:11:58
+ * @LastEditTime: 2025-08-26 12:27:08
  */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
@@ -16,7 +16,6 @@ async function bootstrap() {
   // 动态CORS配置（自动适配开发/生产环境）
   app.enableCors({
     origin: configService.get('CORS_ORIGINS').split(','), // 从环境变量读取
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   });
 
