6792d52a6ec4f5c83f97d1701fe723febfc13db6
yzy <yanziyi290@gmail.com>
2025-08-28 13:22:53 +0100

    11

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,14 @@
+/*
+ * @Author: yzy
+ * @Date: 2025-08-19 21:45:37
+ * @LastEditors: yzy
+ * @LastEditTime: 2025-08-28 13:22:44
+ */
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Welcome to CloudLoom!';
   }
 }
