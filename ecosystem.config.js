f8b58e237f09cb8d241f602e8e5726d0f9b442e8
yzy <yanziyi290@gmail.com>
2025-08-28 12:41:30 +0100

    在服务器上新建了用户cloudloom，以此用户的身份启动服务

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,12 +2,13 @@
  * @Author: yzy
  * @Date: 2025-08-20 11:05:48
  * @LastEditors: yzy
- * @LastEditTime: 2025-08-28 12:17:14
+ * @LastEditTime: 2025-08-28 12:38:37
  */
 module.exports = {
   apps: [{
     name: 'cloudloom-server',
     script: '/current/main.js',
+    user: "cloudloom",
     instances: 'max',
     exec_mode: 'cluster',
     env: {
