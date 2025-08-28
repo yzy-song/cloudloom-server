c7997a922be624f66d5a3a0d4dc1fc5c64ab1813
yzy <yanziyi290@gmail.com>
2025-08-28 13:26:38 +0100

    # 修复 Git 仓库所有权 sudo chown -R $(whoami):$(whoami) /var/www/cloudloom-server/repo.git
    # 或添加安全目录（二选一）
    #git config --global --add safe.directory /var/www/cloudloom-server/repo.git

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,13 +2,13 @@
  * @Author: yzy
  * @Date: 2025-08-19 21:45:37
  * @LastEditors: yzy
- * @LastEditTime: 2025-08-28 13:22:44
+ * @LastEditTime: 2025-08-28 13:26:27
  */
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Welcome to CloudLoom!';
+    return "Welcome to CloudLoom! Let's weave some magic together.";
   }
 }
