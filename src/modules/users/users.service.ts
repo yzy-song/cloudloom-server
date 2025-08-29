3e0cf0206023185802270c8d6bfa09dd06cc1a20
yzy <yanziyi290@gmail.com>
2025-08-30 00:00:06 +0100

    22

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -36,7 +36,7 @@ export class UsersService {
   }
 
   async remove(id: string) {
-    this.logger.log(`Removing user from database : ${id}`);
+    this.logger.log(`Removing user from database with id: ${id}`);
     return this.userRepo.delete(id);
   }
 }
