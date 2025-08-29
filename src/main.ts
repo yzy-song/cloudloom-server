60a2bbb3d5f0a2f5bd7ea5c5208d5de296438434
yzy <yanziyi290@gmail.com>
2025-08-29 07:20:17 +0100

    允许安全的跨域弹窗

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@
  * @Author: yzy
  * @Date: 2025-08-19 21:45:37
  * @LastEditors: yzy
- * @LastEditTime: 2025-08-28 16:22:04
+ * @LastEditTime: 2025-08-29 07:19:36
  */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
@@ -16,9 +16,10 @@ async function bootstrap() {
   // 动态CORS配置（自动适配开发/生产环境）
   app.enableCors({
     origin: configService.get('CORS_ORIGINS').split(','), // 从环境变量读取
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'], // 显式声明
     credentials: true,
     crossOriginOpenerPolicy: {
-      policy: 'same-origin', // 或 'same-origin-allow-popups'（允许弹出窗口）
+      policy: 'same-origin-allow-popups', // 允许安全的跨域弹窗
     },
   });
 
