9ad5b37d5ab39f4d3813f4c4384e75723916c286
yzy <yanziyi290@gmail.com>
2025-08-30 23:55:48 +0100

    数据迁移测试

--- data-source.ts
+++ data-source.ts
@@ -1,11 +1,14 @@
+import * as dotenv from 'dotenv';
 import { DataSource } from 'typeorm';
+
+dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
   host: process.env.DB_HOST || 'localhost',
   port: Number(process.env.DB_PORT) || 5432,
   username: process.env.DB_USERNAME || 'cloudloom',
-  password: process.env.DB_PASSWORD || '',
+  password: String(process.env.DB_PASSWORD || ''),
   database: process.env.DB_DATABASE || 'dbcloudloom',
   entities: ['src/core/entities/*.entity.{ts,js}'],
   migrations: ['src/migrations/*.{ts,js}'],
