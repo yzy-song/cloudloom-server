ded4b11354ac344e44007e0ba08b0fdde7732cad
yzy <yanziyi290@gmail.com>
2026-05-15 10:21:48 +0100

    删除一行，防止报错 [PM2][ERROR] To use --uid and --gid please run pm2 as root

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,20 +5,21 @@
  * @LastEditTime: 2025-08-28 17:11:56
  */
 module.exports = {
-  apps: [{
-    name: 'cloudloom-server',
-    script: './current/main.js',
-    user: "cloudloom",
-    instances: 1,
-    exec_mode: 'fork',
-    env: {
-      NODE_ENV: 'production',
-    },
-    // ----------------------------
+  apps: [
+    {
+      name: 'cloudloom-server',
+      script: './current/main.js',
+      instances: 1,
+      exec_mode: 'fork',
+      env: {
+        NODE_ENV: 'production',
+      },
+      // ----------------------------
 
-    error_file: './logs/err.log',
-    out_file: './logs/out.log',
-    log_file: './logs/combined.log',
-    time: true,
-  }],
+      error_file: './logs/err.log',
+      out_file: './logs/out.log',
+      log_file: './logs/combined.log',
+      time: true,
+    },
+  ],
 };
